aaac77b1cc0316016160d09b355c00f8383f7a5d
Daniel Kukula <daniel.kuku@gmail.com>
2017-12-12 21:21:20 +0000

    Icons in TopMenu

--- src/TopMenu.js
+++ src/TopMenu.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import {Menu, Container} from 'semantic-ui-react';
+import {Menu, Container, Icon} from 'semantic-ui-react';
 import {NavLink} from 'react-router-dom';
 
 const FixedMenu = () => (
     <Menu size='large'>
         <Container>
-            <Menu.Item as={NavLink} to="/">Home</Menu.Item>
+            <Menu.Item as={NavLink} to="/"><Icon name='home'/> Home</Menu.Item>
             <Menu.Menu position="right">
-              <Menu.Item as={NavLink} to="/projects">Projects</Menu.Item>
-              <Menu.Item as={NavLink} to='/about'>About Me</Menu.Item>
-              <Menu.Item as={NavLink} to='/contact'>Contact</Menu.Item>
+              <Menu.Item as={NavLink} to="/projects"><Icon name='code'/>Projects</Menu.Item>
+              <Menu.Item as={NavLink} to='/about'><Icon name='user'/>About Me</Menu.Item>
+              <Menu.Item as={NavLink} to='/contact'><Icon name='mail'/>Contact</Menu.Item>
             </Menu.Menu>
         </Container>
   </Menu>
