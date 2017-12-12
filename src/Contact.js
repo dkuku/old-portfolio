42746d33e2fafc28868e4c82e65cc29290369463
Daniel Kukula <daniel.kuku@gmail.com>
2017-12-12 22:18:25 +0000

    Contact page draft

--- src/Contact.js
+++ src/Contact.js
@@ -1,11 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {Grid} from 'semantic-ui-react';
+import MyHeading from './MyHeading';
+import {Icon, Header, Button} from 'semantic-ui-react';
 
 export default class Contact extends Component {
   render() {
     return (
-      <div>
-        Contact
-      </div>
+   <Grid divided='vertically'>
+    <Grid.Row columns={2}>
+      <Grid.Column>
+        <MyHeading />
+      </Grid.Column>
+      <Grid.Column>
+        <Header as='h3' style={{ margin: '1em' }}>To Contact me please choose:</Header>
+        <Button.Group vertical>
+          <Button color="google plus" as='a' href='mailto:daniel.kuku@gmail.com'>
+          <Icon name="mail" /> Gmail
+          </Button>
+          <Button color='linkedin' as='a' href='https://www.linkedin.com/in/daniel-kukula-55057a57/'>
+          <Icon name='linkedin' /> LinkedIn
+          </Button>
+          <Button color='github' as='a' href='https://github.com/dkuku'>
+            <Icon name='github' /> Github
+          </Button>
+        </Button.Group>
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
     )
   }
 }
