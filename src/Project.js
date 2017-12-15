afa19c9fcf541645dacca464375fd811f3d5334e
Daniel Kukula <daniel.kuku@gmail.com>
2017-12-15 00:41:31 +0000

    converted projects from items to cards

--- src/Project.js
+++ src/Project.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import { Grid, Image, Item, Button } from 'semantic-ui-react';
+import { Card ,Label, Grid, Image, Item, Button } from 'semantic-ui-react';
 import Techlist from './Techlist';
 
 export default class Project extends Component {
@@ -15,27 +15,21 @@ export default class Project extends Component {
         const {title, img, desc, description, technology, link, key} = this.props;
 
         return (
-            <Item>
-              {(this.props.num % 2 === 0) ? <Item.Image as='a' href={link} size='medium' alt={title} src={"images/".concat(img)} /> : null} 
-                <Item.Content>
-                    <Item.Header as='a' href={link}>{title}</Item.Header>
-                    <Item.Meta>{desc}</Item.Meta>
-                    <Item.Description>
-                    {description}
-                    </Item.Description>
-                    <Item.Extra>Technology used: 
-                    <Techlist technology={technology} />
-                    </Item.Extra>
-                    <Button 
-                        as='a'
-                        size='medium'
-                        href={link}
-                        target='_blank'
-                    >
-                        {title}     
-                    </Button>
-                </Item.Content>
-              {this.props.num % 2 === 1 ? <Item.Image as='a' href={link} size='medium' alt={title} src={"images/".concat(img)} /> : null } 
-            </Item>
-    )};
+            <Card 
+            raised
+            href={link}
+            >
+            <Image src={"images/".concat(img)} />
+            <Card.Content>
+            <Label as='a' color='teal' ribbon>{technology[0]}</Label>
+            <Card.Header>{title}</Card.Header>
+            <Card.Meta>{desc}</Card.Meta>
+            <Card.Description>{description}</Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+            <Techlist technology={technology}/>
+            </Card.Content>
+            </Card>
+
+   )};
 };
--- src/Projects.js
+++ src/Projects.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
-import { Header, Container, Item, Grid } from 'semantic-ui-react'
+import { Card, Header, Container, Item, Grid } from 'semantic-ui-react'
 import Project from './Project';
 
 export default class Projects extends Component {
@@ -16,56 +16,63 @@ export default class Projects extends Component {
             title:"Chalets and Caviar",
             img: "Chalets.jpg",
             desc: "Website for Real Estate Agency",
-            description: "Website made using wordpress and some aditional plugins",
+            description: "Website made using wordpress and some additional plugins",
             technology: ["wordpress"],
             link: "http://dev-daniel-project2.pantheonsite.io/"
             },{
             title: "Brooklyn Outdoor Film Festival",
             img: 'Festival.jpg',
             desc: 'Website prototype for Film Festival',
-            description: 'Frontend made using bootstrap',
-            technology: ['bootstrap', "html", "js"],
+            description: 'Frontend made using bootstrap and svg for diagonal sections',
+            technology: ['bootstrap', "html", "js", 'svg'], 
             link: 'projects/prototype/index.html'
             },{
             title: 'Chat app',
             img: 'chat.png',
             desc: 'Chat app written in node.js' ,
-            description: 'Aplication uses node.js express and socket.io',
-            technology: ["node.js", "socket.io"],
+            description: 'Application uses node.js express and socket.io. Couple of users can login to the same room and chat',
+            technology: ["node.js", "socket.io", 'express'],
             link: 'https://chat-node-by-kuku.herokuapp.com/'
             },{
             title: 'Todo App',
             img: 'todo.png',
             desc: 'Simple todo app',
-            description: 'using javascript, jQuery and local storage',
-            technology: ['html', 'css', 'js jQuery'],
+            description: 'using javascript, jQuery and local storage for storing todos between restarts',
+            technology: ['jQuery', 'html', 'css', 'js'],
             link: 'projects/TodoListProject/index.html'
             },{
             title: 'Rick Rocks',
             img: 'Asteroids.jpg',
             desc: 'Asteroids Clone' ,
             description: 'Game made using python and codeskulptor',
-            technology: ["object oriented python"],
+            technology: ["object oriented python", "codeskulptor", 'simple gui'],
             link: 'http://www.codeskulptor.org/#user16_T5YN7P0gBI_6.py'
             },{
             title: 'RGB Game',
             img: 'rgb-game.png',
             desc: 'Color guessing game',
             description: 'Game made using html, css and javascript',
-            technology: ['html', 'css', 'js'],
+            technology: ['js', 'html', 'css'],
             link: 'projects/rgb-color-game.html'
-            } 
+            }, {
+            title: 'Portfolio',
+            img: 'portfolio.jpg',
+            desc: 'Portfolio site',
+            description: 'Made using react, react-router and semantic-ui',
+            technology: ['react', 'react-router', 'semantic-ui'],
+            link: '#'
+            }
         ]}
     render() {
         return (
             <Container>
-            <Item.Group divided style={{ paddingBottom: '1em' }}>
+            <Card.Group>
                 {this.props.projects.map((r, index) => {
                     return (
                         <Project key={index} num={index} {...r} />
                     )
                 })}
-            </Item.Group>
+            </Card.Group>
             </Container>
         )
     }
